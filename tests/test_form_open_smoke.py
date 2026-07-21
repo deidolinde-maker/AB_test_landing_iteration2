72fe15ad13c32daec8e18bc81fc53f99d61f577b
deidolinde-maker <deidolinde@gmail.com>
2026-07-21 17:31:12 +0300

    Set theme_ab_variant before search AB

--- tests/test_form_open_smoke.py
+++ tests/test_form_open_smoke.py
@@ -1,7 +1,7 @@
 import pytest
 
 from components.address_form import AddressForm
-from helpers.ab_cookie import assert_ab_cookie_value, set_ab_cookie
+from helpers.ab_cookie import assert_ab_cookie_value, set_ab_cookie, set_theme_ab_cookie
 from pages.landing_page import LandingPage
 
 pytestmark = [pytest.mark.e2e, pytest.mark.smoke, pytest.mark.form_open_smoke]
@@ -14,6 +14,7 @@ def test_form_open_smoke(case, page, context, site_config_map, form_config_map):
     form = AddressForm(page, form_config)
 
     target_url = site_config.urls[case.url_type]
+    set_theme_ab_cookie(context, target_url, "a")
     set_ab_cookie(context, target_url, case.variant)
     landing.open(case.url_type)
     assert_ab_cookie_value(context, case.variant)
